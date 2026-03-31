03328ceec6ce130d35a6f938e8f7930ef58d98d9
student-prashan <12211096cse@gmail.com>
2026-03-31 17:30:56 +0530

    create and test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -12,3 +13,10 @@ mongoose.connect(process.env.MONGO).then(()=>{
 const app=express();
 
 app.listen(3000);
+
+app.get('/test', (req, res)=>{
+    res.json({
+        message:'Hello World!',
+    });
+});
+app.use('/api/user',userRouter)
